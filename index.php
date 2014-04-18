7cad69a8ff50c1b759d3425f257266d0d61e4f92
Hiroyuki Takahashi <hiro@mrhiro.com>
2014-04-18 10:13:52 -0600

    Bug Fixes

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-					<h1><?php the_title(); ?></h1>
+					<h1 id="post-title"><?php the_title(); ?></h1>
 
 					<?php the_content(); ?>
 
